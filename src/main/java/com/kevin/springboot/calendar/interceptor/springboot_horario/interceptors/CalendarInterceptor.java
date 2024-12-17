3a1ea87edb8c0eadaca53110fe4a17c0d3881bc8
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2024-12-16 23:58:10 -0500

    Feat: Creando interceptor horario

---
+++ src/main/java/com/kevin/springboot/calendar/interceptor/springboot_horario/interceptors/CalendarInterceptor.java
@@ -0,0 +1,29 @@
+package com.kevin.springboot.calendar.interceptor.springboot_horario.interceptors;
+
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CalendarInterceptor implements HandlerInterceptor{
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable ModelAndView modelAndView) throws Exception {
+        // TODO Auto-generated method stub
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        // TODO Auto-generated method stub
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    
+}
